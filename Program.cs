d9e3b9d99de2ee78bb1a57ee7584efaa0e1a5f50
Michael Gustavsson <michael.gustavsson@softtech-dev.se>
2026-09-11 09:40:47 +0200

    Added a new method

--- Program.cs
+++ Program.cs
@@ -5,5 +5,11 @@ class Program
     static void Main()
     {
         Console.WriteLine("Hello, Git!");
+        Console.WriteLine(DoSomething("Michael"));
+    }
+
+    static string DoSomething(string name)
+    {
+        return $"Hello {name}";
     }
 }
